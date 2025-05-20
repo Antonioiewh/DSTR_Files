4039b269fa106c06d34a68fe0e34b1bd72df8630
Antonioiewh <nghwei1@gmail.com>
2025-05-20 14:05:04 +0800

    Update tut 1B.py

--- week 2 code/tut 1B.py
+++ week 2 code/tut 1B.py
@@ -10,7 +10,11 @@ def minmax(data):
             big = value
 
     return (small, big)
-
+#NOT allowed to use min max so we will use this instead
+# First statement assigns dta[0] to BOTH small and big variables as obviously you need something to compare with 
+# Next you wil check through the data list
+# if value < small , small = vlaue means if value less than variable small, you will proceed to pass the current value you're looking at into the variable small
+# if value > big , big = vlaue means if value larger than variable big, you will proceed to pass the current value you're looking at into the variable big
 #qestion 2
 def sum_of_squares(n):
     total = 0
@@ -18,7 +22,12 @@ def sum_of_squares(n):
         total += j * j
     return total
 
-#question 3
+print(sum_of_sqaures(n)) 
+# first assign value of total var = 0
+# for j in range (1 n + 1) means looking at values less or equal. n+1 allows you to look at the value n as n+1 is the limit. 
+# total += j*j means you will calculate the square of j and add it to total
+# you need to use print() to display the value of total from the function
+
 def sum_of_squares(n):
     total = 0
     for j in range(1, n + 1, 2):
