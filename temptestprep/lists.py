2715bea16e0aba267e93e2b2abc92146efe810d3
Antonioiewh <nghwei1@gmail.com>
2025-05-21 10:39:24 +0800

    Update lists.py

--- temptestprep/lists.py
+++ temptestprep/lists.py
@@ -13,47 +13,54 @@ print(list[::-1])  # Output: [5, 4, 3, 2, 1]
 
 
 # Examples 
-print(f'Example 1 with it being list [::-2]: {list[::-2]}')  # Output: [5, 3, 1]
+print(f'Example 1 with it being list [::-2]: {list[::-2]}')  
 # Explanation
 # Include element at index -2 and skip over every 2nd element when going backwards 
 
 # Example 2
-print(f'Example 2 with it being list [-2::]: {list[-2::]}')   # Output: [1, 3, 5]
+print(f'Example 2 with it being list [-2::]: {list[-2::]}')   
 # Explanation
-# Include element at index -2 and print til end
+# Start at element at at index -2 and print til end
 
 # Example 3
 print(f'Exaple 3 with it being list [:-2] {list[:-2]}')
-#Explanation start from the element after -2 which is 3 till the end
-
+# Print till element at index -2
 # Example 4
-print(f'Exaple 4 with it being list [:-2] {list[-2:]}')
-#Explanation start from element at -2 till the end
-
+print(f'Exaple 4 with it being list [-2:] {list[-2:]}')
+# Explanation 
+# Start at element at index -2, print till end
 
 # Example 5
-print(f'Example 3 with it being list [::2]: {list[::2]}')
+print(f'Example 5 being list [-2::-2] {list[-2::-2]}')
+# Explanation
+# Start at index -2, step by -2 
+# Example 6
+print(f'Example 5 being list [-2::-2] {list[-2::2]}')
+# Start at index -2, step by 2
+
+# Example 7
+print(f'Example 7 with it being list [::2]: {list[::2]}')
 # Explanation
 # Include element at index 2 and skip over every 2nd element when going backwards
 
-#Example 6
-print(f'Example 6 with it being list[2::] {list[2::]}')
+#Example 8
+print(f'Example 8 with it being list[2::] {list[2::]}')
 # Explanation
 # Include element at index 2 and print til end
 
-#Example 7
-print(f'Example 7 with it being list[:2] {list[:2]}')
+#Example 9
+print(f'Example 9 with it being list[:2] {list[:2]}')
 # Explanation
-# Exclude element at index 2 and print till end
+# Print till element at 2
 
 
-#Example 8
-print(f'Example 8 with it being list[2:] {list[2:]}')
+#Example 10
+print(f'Example 11 with it being list[2:] {list[2:]}')
 # Explanation
-# Include element at index 2 and print till end
+# Start at element at index 2 , print till end
 
 
-#Example 5
+#Example 11
 #Turn "the quick brown fox!!" turn into "quick brown fox!"
 #Options ( multi select)
 # [4:-1:-1]
